5052f2860cab99f41e30b7c306c8987662b4c4ab
sunu <lsw2protocol@gmail.com>
2023-03-31 13:48:49 +0900

    chat fixed style1

--- pages/chat/subject.tsx
+++ pages/chat/subject.tsx
@@ -8,7 +8,7 @@ const Subject: NextPage = () => {
   const onSubmit = (event: any) => {
     event?.preventDefault();
     setChats([...chats, chat]);
-
+    setChat("");
     console.log(chats);
   };
 
@@ -20,8 +20,18 @@ const Subject: NextPage = () => {
   };
 
   return (
-    <div className="fixed w-screen h-screen flex flex-col items-center justify-center">
-      <div className="w-full absolute bottom-10 left-5 right-5">
+    <div className="w-screen flex flex-col">
+      <ul className=" pb-20 my-5 space-y-5 overflow-y-auto">
+        {chats.map((chat, i) => (
+          <li
+            key={i}
+            className="py-3 px-5 text-right bg-slate-500 rounded-full rounded-br-none shadow-md"
+          >
+            <h4 className="break-words text-white">{chat}</h4>
+          </li>
+        ))}
+      </ul>
+      <div className="w-full fixed bottom-10 left-5 right-5">
         <form onSubmit={onSubmit}>
           <input
             onChange={onChange}
@@ -29,24 +39,14 @@ const Subject: NextPage = () => {
             type="text"
             placeholder="What's on your mind?"
             maxLength={120}
-            className="appearance-none text-gray-400 w-9/12 px-3 py-2 mr-3 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-yellow-500 focus:border-yellow-500"
+            className="appearance-none shadow-md text-gray-400 w-9/12 px-3 py-2 mr-3 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:ring-yellow-500 focus:border-yellow-500"
           />
           <input
             type="submit"
             value="Enter"
-            className="mt-5 bg-yellow-500 hover:bg-yellow-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-yellow-700 focus:outline-none"
+            className="mt-5 bg-yellow-500 hover:bg-yellow-600 text-white py-2 px-4 border border-transparent rounded-md shadow-md text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-yellow-700 focus:outline-none"
           />
         </form>
-      </div>
-      <div className=" space-y-5">
-        {chats.map((chat, i) => (
-          <div
-            key={i}
-            className="py-3 px-5 bg-slate-500 rounded-lg rounded-br-none"
-          >
-            <h4 className=" text-white">{chat}</h4>
-          </div>
-        ))}
       </div>
     </div>
   );
